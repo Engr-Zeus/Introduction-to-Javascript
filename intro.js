651177e91e580a9cb137977446bb153b4dc18f36
Engr Zeus Kingsley <engrkingsley@live.com>
2022-06-24 01:45:33 +0100

    First Commit

---
+++ intro.js
@@ -0,0 +1,6 @@
+let myName = "Kingsley Andrew Onwubuya";
+let myHeight = 6;
+let myCountry = "Nigeria";
+
+alert("My name is "+ myName +"." + "\nI am " + myHeight + "ft tall." + "\nI am from " + myCountry + "." )
+
